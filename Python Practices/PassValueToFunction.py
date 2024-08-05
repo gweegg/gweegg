36ad6c75693b5d2038493876a7152f7ed904c980
gweegg <joshua.gregorio0116@gmail.com>
2024-08-05 11:57:38 +0000

    pass value to a function 2

--- Python Practices/PassValueToFunction.py
+++ Python Practices/PassValueToFunction.py
@@ -8,3 +8,11 @@ def print_test(text):
     print("The output is: " , text)
 
 print_test(my_text)
+
+#ass value to a function 2
+def InputAge(age):
+    new_age = age + 10
+    return new_age
+
+How_old = InputAge(3)
+print(How_old) 
